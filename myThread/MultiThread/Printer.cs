59727460254bf9f1025f301073b375bf016893f1
Nadia Regina de Oliveira <nadia.oliveira@bgmrodotec.com.br>
2018-06-14 15:56:26 -0300

    Comitando as threads.

---
+++ myThread/MultiThread/Printer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MultiThread
+{
+    class Printer
+    {
+        //public void PrintNumbers() //Utilizando Lock
+        //{
+        //    lock (this)
+        //    {
+        //        for (int i = 0; i < 5; i++)
+        //        {
+        //            Thread.Sleep(100);
+        //            Console.WriteLine(i + ",");
+        //        }
+        //        Console.WriteLine();
+
+        //    }
+
+        //}
+        public void PrintNumbers() //Utilizando monitor type 
+        {
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write(i + ",");
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Monitor.Exit(this);
+            }
+            
+
+        }
+    }
+}
---
+++ myThread/MultiThread/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****MultiThreads******");
+            Printer p = new Printer();
+            Thread[] threads = new Thread[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = "threadFilha" + i;
+            }
+
+            foreach (Thread t in threads)
+                t.Start();
+            Console.Read();
+        }
+    }
+}
